bed03561aecf95c320ff6e77a74dc135a58db0b4
spitfire21 <cody_bot@hotmail.com>
2016-02-23 10:50:32 -0800

    Sample client for use with the server, can be used as a base for the server.

---
+++ Java-Game-Client/src/client/Client.java
@@ -0,0 +1,49 @@
+package client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client {
+	public static void main (String[] args) throws UnknownHostException, IOException{
+		Scanner scan = new Scanner(System.in);
+		Socket sock = null;
+		String server = scan.nextLine();
+		int port = scan.nextInt();
+		
+		try {
+		    sock = new Socket(server, port);
+		    PrintWriter out =
+		        new PrintWriter(sock.getOutputStream(), true);
+		    BufferedReader in =
+		        new BufferedReader(
+		            new InputStreamReader(sock.getInputStream()));
+		    BufferedReader stdIn =
+			        new BufferedReader(
+			            new InputStreamReader(System.in));
+		    
+		    String userInput;
+		    String serverInput;
+		    while(true){
+		    	if(in.ready()){
+		    		serverInput = in.readLine();
+		    		System.out.println("ECHO: " + serverInput);
+		    		
+		    	}
+		    	if(stdIn.ready()) {
+		    		userInput = stdIn.readLine();
+		    			out.println(userInput);
+		        
+		    	}
+		    	
+		    }
+		
+		} finally {
+			sock.close();
+		}
+	}
+}
