d2bce2c0e51c6a366da3016c07d55e36b3ddf3ea
Joshua Wiersma <wiersmjl@plu.edu>
2016-04-01 15:12:09 -0700

    Revamped rules page, locked windows, GUI enhancements.

---
+++ farkle-client/src/main/java/GUI/FarkleProbabilities.java
@@ -0,0 +1,75 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JDesktopPane;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JTextPane;
+import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class FarkleProbabilities extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FarkleProbabilities frame = new FarkleProbabilities();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FarkleProbabilities() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 845, 489);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		desktopPane.setBackground(new Color(128, 0, 0));
+		contentPane.add(desktopPane, BorderLayout.CENTER);
+		
+		JButton button = new JButton("Back");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Rules rules = new Rules(null);
+				rules.setVisible(true);
+				setVisible(false);
+			}
+		});
+		button.setFont(new Font("Bodoni 72 Smallcaps", Font.PLAIN, 20));
+		button.setBounds(337, 327, 160, 71);
+		desktopPane.add(button);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(41, 21, 752, 282);
+		desktopPane.add(scrollPane);
+		
+		JTextPane txtpnFarkleProbabilities = new JTextPane();
+		scrollPane.setViewportView(txtpnFarkleProbabilities);
+		txtpnFarkleProbabilities.setFont(new Font("Bodoni 72", Font.PLAIN, 17));
+		txtpnFarkleProbabilities.setText("FARKLE PROBABILITIES:\n\nFollowing are the probabilities of scoring combinations in the initial throw of six dice.\n\nDice combination\tProbability\nThree of a kind\t1 in 3.240\nFour of a kind\t1 in 20.736\nThree pair\t1 in 25.92\nStraight\t1 in 64.8\nTwo triplets\t1 in 155.52\nFive of a kind\t1 in 259.2\nSix of a kind\t1 in 7776\n\nFor the most part, less probable combinations are scored higher than more probable combinations (see Scoring Variations).\n\nFollowing are the probabilities of scoring combinations in subsequent throws of the dice.\n\nDice combination\tFive dice left\tFour dice left\tThree dice left\nThree of a kind\t1 in 5.184\t1 in 10.8\t1 in 36\nFour of a kind\t1 in 51.84\t1 in 216\tN/A\nFive of a kind\t1 in 1296\tN/A\tN/A\nFollowing are the probabilities of farkling if all variant scoring combinations are allowed, depending on the number of dice thrown.\n\nDice thrown\tProbability\n6\t1 in 42\n5\t1 in 13\n4\t1 in 6.35 (20 in 127)\n3\t1 in 3.6 (5 in 18)\n2\t1 in 2.25 (4 in 9)\n1\t1 in 1.5 (2 in 3)\n\nThree-pair is the only scoring variation that alters the likelihood of farkling, and only on the initial throw of six dice. If three pairs are not scored, the probability of farkling on the initial throw increases to 1 in 32.\n\nFollowing are the probabilities of making hot dice in a single throw if all variant scoring combinations are allowed, depending on the number of dice thrown.\n\nDice thrown\tProbability\n6\t1 in 12\n5\t1 in 32\n4\t1 in 25\n3\t1 in 18\n2\t1 in 9\n1\t1 in 3\n\nIf no variant scoring combinations are allowed, the probabilities of making hot dice are decreased only slightly for 4-6 dice thrown, and unchanged for 1-3 dice thrown. Odds for these and other die combinations with explanations and simulation results can be found elsewhere.[11][12]\n\n");
+	}
+
+}
---
+++ farkle-client/src/main/java/GUI/ScoringVariations.java
@@ -0,0 +1,75 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JDesktopPane;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JTextPane;
+import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ScoringVariations extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ScoringVariations frame = new ScoringVariations();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ScoringVariations() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 845, 489);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		desktopPane.setBackground(new Color(128, 0, 0));
+		contentPane.add(desktopPane, BorderLayout.CENTER);
+		
+		JButton button = new JButton("Back");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Rules rule = new Rules(null);
+				rule.setVisible(true);
+				setVisible(false);
+			}
+		});
+		button.setFont(new Font("Bodoni 72 Smallcaps", Font.PLAIN, 20));
+		button.setBounds(337, 337, 160, 71);
+		desktopPane.add(button);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(68, 6, 698, 316);
+		desktopPane.add(scrollPane);
+		
+		JTextPane txtpnScoringVariationsSince = new JTextPane();
+		scrollPane.setViewportView(txtpnScoringVariationsSince);
+		txtpnScoringVariationsSince.setText("SCORING VARIATIONS:\n\nSince farkle is a folk game, variant rules are used in different playing communities. While the standard rules described above are widely used, even they are not universal. For example, the commercially marketed game of Pocket Farkel differs in that three 1s are scored as 300 rather than 1000.[6] In addition, some players score one or more combinations of dice beyond the standard ones. Those variations include the following.\n\nNo scoring dice are rolled (e.g. 2-2-3-4-6-6) is scored as 500.\n\nThree pair (e.g., 1-1-4-4-6-6) is scored as 500, 600, 750, 1000, or 1500.\n\nA straight (1-2-3-4-5-6) is scored as 1000, 1200, 1500, or 2500.\n\nA full house (three of a kind and a pair) is scored as the three of a kind value plus 250. e.g. 3-3-3-2-2 = 550, 4-4-4-3-3 = 650, 5-5-5-1-1 = 750, 1-1-1-3-3 = 1250\n\nFour, five and six of a kind are scored in one of 3 ways: adding, doubling or set value:\nAdding, for each additional matching die above 3 of a kind, the 3 of a kind score is added. e.g. 3-3-3 = 300, 3-3-3-3 = 300 + 300 (600), 3-3-3-3-3 = 300 + 300 + 300 (900) and 3-3-3-3-3-3 = 300 + 300 + 300 + 300 (1200).\n\nDoubling, for each additional matching die above 3, 4 or 5 of a kind the score is doubled. e.g. 3-3-3 = 300, 3-3-3-3 = 300 × 2 (600), 3-3-3-3-3 = 300 × 2 × 2 (1200) and 3-3-3-3-3-3 = 300 × 2 × 2 × 2 (2400)\n\nSet value, 4 of a kind is scored as 1000 or 2000, 5 of a kind is scored as 2000 or 4000 and 6 of a kind is scored as 3000, 6000 or 10000\n\nDice value\t3 of a kind\t\n\nAdding\tDoubling\tSet value\n4 of a kind\t5 of a kind\t6 of a kind\t4 of a kind\t5 of a kind\t6 of a kind\t4 of a kind\t5 of a kind\t6 of a kind\nTwo\t200\t400\t600\t800\t400\t800\t1600\t1000 or 2000\t2000 or 4000\t3000, 6000 or 10000\nOne (low)[6]\t300\t600\t900\t1200\t600\t1200\t2400\nThree\t300\t600\t900\t1200\t600\t1200\t2400\nFour\t400\t800\t1200\t1600\t800\t1600\t3200\nFive\t500\t1000\t1500\t2000\t1000\t2000\t4000\nSix\t600\t1200\t1800\t2400\t1200\t2400\t4800\nOne (high)\t1000\t2000\t3000\t4000\t2000\t4000\t8000\nLike the standard combinations, any of these variant combinations must be achieved in a single throw.\n\nThese are the variations listed in the above-cited descriptions of farkle scoring, but further variations presumably exist. Since it is a folk game, players are free to agree upon whatever scores they choose for whatever combinations they choose to recognize.");
+		txtpnScoringVariationsSince.setFont(new Font("Bodoni 72", Font.PLAIN, 18));
+	}
+
+}
