aecc86ef6270e4505b865a1a12885a0a6d215281
Joshua Wiersma <wiersmjl@plu.edu>
2016-04-03 13:14:41 -0700

    Updated Settings Page

--- farkle-client/src/main/java/GUI/Settings.java
+++ farkle-client/src/main/java/GUI/Settings.java
@@ -43,7 +43,7 @@ public class Settings extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Settings(GameMainMenu frame) {
+	public Settings(final GameMainMenu frame) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 845, 489);
 		contentPane = new JPanel();
@@ -55,60 +55,39 @@ public class Settings extends JFrame {
 		desktopPane.setBackground(new Color(128, 0, 0));
 		contentPane.add(desktopPane, BorderLayout.CENTER);
 		
-		JList list = new JList();
-		list.setModel(new AbstractListModel() {
-			String[] values = new String[] {"800x600", "1024x768", "1280x720", "1920x1080", "2560x1440"};
-			public int getSize() {
-				return values.length;
-			}
-			public Object getElementAt(int index) {
-				return values[index];
-			}
-		});
-		list.setBounds(512, 27, 88, 85);
-		desktopPane.add(list);
-		
-		JLabel lblResolution = new JLabel("Resolution:");
-		lblResolution.setForeground(new Color(255, 255, 255));
-		lblResolution.setHorizontalAlignment(SwingConstants.CENTER);
-		lblResolution.setFont(new Font("Bodoni 72 Smallcaps", Font.PLAIN, 20));
-		lblResolution.setBounds(210, 51, 117, 29);
-		desktopPane.add(lblResolution);
-		
-		JLabel lblNetwork = new JLabel("Network:");
-		lblNetwork.setForeground(new Color(255, 255, 255));
-		lblNetwork.setFont(new Font("Bodoni 72 Smallcaps", Font.PLAIN, 20));
-		lblNetwork.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNetwork.setBounds(210, 145, 117, 29);
-		desktopPane.add(lblNetwork);
-		
-		JLabel lblAccount = new JLabel("Account:");
-		lblAccount.setForeground(new Color(255, 255, 255));
-		lblAccount.setFont(new Font("Bodoni 72 Smallcaps", Font.PLAIN, 20));
-		lblAccount.setHorizontalAlignment(SwingConstants.CENTER);
-		lblAccount.setBounds(210, 186, 117, 29);
-		desktopPane.add(lblAccount);
-		
 		JButton btnBack = new JButton("Back");
+		btnBack.setFont(new Font("Bodoni 72 Smallcaps", Font.PLAIN, 20));
 		btnBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setVisible(false);
-			
 				frame.setVisible(true);
 			}
 		});
-		btnBack.setBounds(85, 338, 156, 68);
+		btnBack.setBounds(339, 338, 156, 68);
 		desktopPane.add(btnBack);
 		
-		JButton btnSavequit = new JButton("Save/Quit");
-		btnSavequit.addActionListener(new ActionListener() {
+		JButton btnAdjustScoringVariations = new JButton("Adjust Scoring");
+		btnAdjustScoringVariations.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				AdjustScoring adjust = new AdjustScoring();
+				adjust.setVisible(true);
 				setVisible(false);
-			
-				frame.setVisible(true);
 			}
 		});
-		btnSavequit.setBounds(574, 338, 156, 68);
-		desktopPane.add(btnSavequit);
+		btnAdjustScoringVariations.setFont(new Font("Bodoni 72 Smallcaps", Font.PLAIN, 20));
+		btnAdjustScoringVariations.setBounds(160, 132, 156, 68);
+		desktopPane.add(btnAdjustScoringVariations);
+		
+		JButton btnAdjustGameplay = new JButton("Adjust Gameplay");
+		btnAdjustGameplay.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AdjustGameplay adjustG = new AdjustGameplay();
+				adjustG.setVisible(true);
+				setVisible(false);
+			}
+		});
+		btnAdjustGameplay.setFont(new Font("Bodoni 72 Smallcaps", Font.PLAIN, 20));
+		btnAdjustGameplay.setBounds(512, 132, 156, 68);
+		desktopPane.add(btnAdjustGameplay);
 	}
 }
